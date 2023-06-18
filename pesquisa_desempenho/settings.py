91a384c33b1e63320fad6856e339c0e72f8b8af0
Elidomar Luiz dos Santos <elidomarsantos@gmail.com>
2023-06-18 17:48:19 -0300

    update2

--- pesquisa_desempenho/settings.py
+++ pesquisa_desempenho/settings.py
@@ -82,11 +82,14 @@ WSGI_APPLICATION = 'pesquisa_desempenho.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RGphgsAZ8AichaVVDyje',
+        'HOST': 'containers-us-west-159.railway.app',
+        'PORT': '6835',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
