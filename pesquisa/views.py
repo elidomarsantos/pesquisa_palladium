820ddd6acbe2aa3df995723cd7593745e12a59af
Elidomar Luiz dos Santos <elidomarsantos@gmail.com>
2023-06-18 20:04:41 -0300

    update 11

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -7,7 +7,7 @@ from .forms import Form_Pesquisa
 from django.shortcuts import redirect, render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-@login_required
+
 def home(request):
     return render(request, 'cp/home.html') 
 
